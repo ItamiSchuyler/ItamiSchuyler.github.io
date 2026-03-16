e06387938f2f4ac12787088ddb8a6c2e405e875b
Itami <89473133+ItamiSchuyler@users.noreply.github.com>
2026-03-16 11:48:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+window.closeTodo = function(){
+  document.getElementById("todoWidget").style.display="none";
+}
+
+/* TODO TODO TODO */
+
+/* PEGA TODO TU SCRIPT COMPLETO */
+
+});
